6a673bf6e61c155b16f38ecd84a5f98d9074850b
Xavier Salazar <x.salazar1729@gmail.com>
2020-12-24 22:23:19 -0800

    Update footer styling

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div
-          style={{
-            gridTemplateRows: "1fr auto",
-            minHeight: "100%",
-            display: "grid",
-          }}
-        >
+        <div className="flex-wrapper">
           <Emoji />
           <Footer />
         </div>
